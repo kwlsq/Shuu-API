aa9a72d74ffc19eabeb0efaba9e803b2610d2225
Vincent <vincentiussss@gmail.com>
2020-02-18 16:25:43 +0700

    send email verif success, create verif page in UI

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,12 +1,12 @@
-const connection = require('../Databases')
-const { createJWToken } = require('../Helpers/jwt')
+const connection = require('../Databases');
+const { createJWToken } = require('../Helpers/jwt');
 const Crypto = require('crypto');
+const transporter = require('../Helpers/nodemailer');
 
 module.exports = {
     login: (req, res) => {
         let { username, password } = req.body
         const hashPassword = Crypto.createHmac('sha256', 'shuu').update(password).digest('hex')
-        console.log(hashPassword)
         const sql = `SELECT * FROM users WHERE username = '${username}' and password = '${hashPassword}'`
         connection.query(sql, (err, results) => {
             if (err) {
@@ -25,7 +25,6 @@ module.exports = {
         })
     },
     keepLogin: (req, res) => {
-        console.log(req.user)
         return res.status(200).send(req.user)
     },
     getByUsername: (req, res) => {
@@ -43,12 +42,41 @@ module.exports = {
         console.log(req.body)
         var { username, password, email, id_role } = req.body
         const hashPassword = Crypto.createHmac('sha256', 'shuu').update(password).digest('hex')
+        console.log(hashPassword)
         const sql = `INSERT INTO users(username, password, email, id_role) values ('${username}','${hashPassword}','${email}','${id_role}')`
-        connection.query(sql, (err) => {
+        connection.query(sql, (err, results) => {
             if (err) {
+                console.log('error1 ')
                 res.status(500).send(err)
             }
+            const sql2 = `SELECT * FROM users WHERE username = '${username}'`
+            connection.query(sql2, (err, results2) => {
+                if (err) {
+                    console.log('error2')
+                    res.status(500).send(err)
+                }
+                let verificationLink = `http://localhost:3000/verified?username=${username}&password=${hashPassword}`
+                let mailOptions = {
+                    from: 'Admin <vincentiussss@gmail.com',
+                    to: 'vincentiussss@gmail.com',
+                    subject: 'Confirmation Email',
+                    html: `
+                        <h2>Hi, This is a verification mail</h2>
+                        <h3>Please click the link below to verified your account</h3>
+                        <a href='${verificationLink}'>click this link!</a>
+                        `
+                }
+                transporter.sendMail(mailOptions, (err, results3) => {
+                    if (err) {
+                        res.status(500).send(err)
+                    }
+                    console.log('yak pulang')
+                    console.log(results3)
+                    res.status(200).send(results3)
+                })
+            })
             console.log(results)
+
             res.status(200).send(results)
         })
     }
