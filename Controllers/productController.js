b076ab0c83fca7c20a21b60c1e72181bc9fa3c23
Vincent <vincentiussss@gmail.com>
2020-03-06 18:54:21 +0700

    get detail product

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -2,8 +2,9 @@ const connection = require('../Databases')
 
 module.exports = {
     showcase: (req, res) => {
-        const sql = `SELECT p.id,p.name,p.price,p.stock,p.image,b.name AS brands,b.profilepic 
+        const sql = `SELECT p.id,pn.name,b.name AS brands,p.image,b.profilepic,p.price,p.stock,p.views 
         FROM products p 
+        JOIN product_name pn ON p.product_name_id=pn.id 
         JOIN brands b ON p.store_id=b.id;
         `
 
@@ -15,5 +16,28 @@ module.exports = {
             console.log(results)
             res.status(200).send(results)
         })
-    }
+    },
+    detailProduct: (req, res) => {
+        console.log(req.body)
+        const sql = `SELECT p.id,pn.name,b.name AS brands,p.image,b.profilepic,p.price,p.stock,p.views 
+        FROM products p 
+        JOIN product_name pn ON p.product_name_id=pn.id 
+        JOIN brands b ON p.store_id=b.id
+        WHERE p.id=${req.body.id};
+        `
+        connection.query(sql, (err, results) => {
+            if (err) {
+                console.log(err)
+                return res.status(500).send(err)
+            }
+            console.log(results)
+            res.status(200).send(results[0])
+        })
+    },
+    availableColor: (req, res) => {
+        // const sql = `SELECT `
+    },
+    availableSize: (req, res) => {
+
+    },
 }
--- Routers/productRouter.js
+++ Routers/productRouter.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const { productController } = require('../Controllers')
-// const { auth } = require('../Helpers/auth')
+const { auth } = require('../Helpers/auth')
 
 
 router.get('/showcase', productController.showcase)
+router.post('/detailproduct', productController.detailProduct)
 
 module.exports = router
