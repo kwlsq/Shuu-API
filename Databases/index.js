36e07111b3d1decbf3f0fc18043762ce6230d87b
Vincent <vincentiussss@gmail.com>
2020-04-20 11:23:20 +0700

    push to heroku

--- Databases/index.js
+++ Databases/index.js
@@ -1,11 +1,22 @@
+// const mysql = require('mysql');
+
+// const connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'Vincentius',
+//   password : 'Vincent1234',
+//   database : 'shuu',
+//   port     : 3306
+// });
+
+// module.exports = connection
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'Vincentius',
-  password : 'Vincent1234',
-  database : 'shuu',
-  port     : 3306
+  host: 'db4free.net',
+  user: 'vincentius',
+  password: 'skeletonspyro',
+  database: 'kwlsqxx',
+  port: 3306
 });
 
 module.exports = connection
